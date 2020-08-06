b45e2850e214d06f7db5fd00ef269805b1979ff6
Prechar <pxiong037@gmail.com>
2020-08-06 10:27:29 +0000

    Updates

---
+++ precache-manifest.213820605b979a66ef54eb1a4be13455.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "314700f01524a2f230325700035d533e",
+    "url": "/coin-flip-react/index.html"
+  },
+  {
+    "revision": "3425401678595b7f1182",
+    "url": "/coin-flip-react/static/css/main.7cd453cb.chunk.css"
+  },
+  {
+    "revision": "9f7e949c189854ba9c64",
+    "url": "/coin-flip-react/static/js/2.3f115486.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coin-flip-react/static/js/2.3f115486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3425401678595b7f1182",
+    "url": "/coin-flip-react/static/js/main.15be4436.chunk.js"
+  },
+  {
+    "revision": "bbc226d9a0bc3874dfd9",
+    "url": "/coin-flip-react/static/js/runtime-main.6230e6ca.js"
+  },
+  {
+    "revision": "99a107299db97ffe8a1491d5427ba596",
+    "url": "/coin-flip-react/static/media/heads.99a10729.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coin-flip-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "43f803d7f5b9706cade56fa3733a8e39",
+    "url": "/coin-flip-react/static/media/tails.43f803d7.jpg"
+  }
+]);
